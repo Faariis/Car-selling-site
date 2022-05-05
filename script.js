e5add96f8ce2edc926eb51a18e3315165019f518
Faariis <ftrtak@gmail.com>
2022-05-05 23:56:53 +0200

    Mala promjena
    Pokušaj namještanja push funkcije.

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const searchInput = document.querySelector("[data_search]");
 
 let users = [] //Čuva podatke u nizu (Prilikom unosa)
 
+
 searchInput.addEventListener("input", (e) => { //Sve što se unese u pretraživać bilježi se u konzoli
     const value = e.target.value.toLowerCase(); //toLowerCase prebaci u mala slova ako npr. unese se veliko slovo
     users.forEach(user => {
         const isVisible = user.name.toLowerCase().includes(value) || user.manufacturer.toLowerCase().includes(value); //Ako bilo gdje u nazivu/proizvođaču ima slovo uneseno, vraća true
         user.element.classList.toggle("hide", !isVisible); //Ako je vidljiv neće biti skriven
     })
-
 })
 
 fetch("https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars")
@@ -33,3 +33,50 @@ fetch("https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars")
         return { name: user.name, manufacturer: user.manufacturer, element: card }
     })
 })
+    
+
+/* POST */
+
+fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars', {
+    method: "POST",
+    headers: {
+        "Content-type": "application/json" //Govori severu koji format podataka šaljemo
+    },
+    body: JSON.stringify(
+        {
+            "id": 7,
+            "name": "Audi",
+            "manufacturer": "Q7",
+            "imageUrl": "8990",
+            "price": 100000,
+            "year": 2020
+          }
+    )      
+})
+    
+ /*   
+const postData = {
+    "id": 7,
+    "name": "Audi",
+    "manufacturer": "Q7",
+    "imageUrl": "8990",
+    "price": 100000,
+    "year": 2020
+};
+try {
+    const response = await fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Cars', {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postData)        
+    });
+    if (!response.ok) {
+        const message = "Error with Status Code: " + response.status;
+        throw new Error(message);
+    }
+    const data = await response.json();
+    console.log(data);
+} catch (error) {
+    console.log("Error: " + err);
+}*/
